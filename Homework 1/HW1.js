61067c99cb5d471c667e88f9e850881b66a60ccd
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2023-01-22 15:47:54 +0200

    Add files via upload

---
+++ Homework 1/HW1.js
@@ -0,0 +1,82 @@
+// task 2
+const myName = "Mariia"
+console.log(myName);
+
+//task 3
+//3a
+let x;
+let y;
+let c;
+
+//task 4
+//3b
+x = 3
+y = true
+
+//3c
+alert(x);
+alert(y);
+
+//3d
+c = x;
+x = y;
+y = c;
+
+// task4
+let idNumbder;
+
+let guitar= {
+name1: 'Ibanez', //string
+isAvailableforSale: true, //boolean
+ipNumber, //undefined
+cost: null //null
+};
+console.log(typeof name1);
+console.log(typeof isAvailableforSale);
+console.log(typeof ipNumber);
+console.log(typeof cost);
+
+// task5
+
+let isAdult = confirm("Are you over 18 years old?");
+alert(isAdult);
+
+// task6
+let name2;
+let lastName;
+let isgroupAssigned;
+let birthYear;
+
+name2 = "Mariia"; //string
+lastName = "Bolonkina"; //string
+group = "true"; //boolean
+birthYear = 1999; //number
+
+let notMarried = true; //boolean
+
+console.log(birthYear, notMarried, name2, lastName, isgroupAssigned);
+
+let programmer;
+let JSexpert = null;
+console.log(typeof programmer);
+console.log(typeof JSexpert);
+
+// task7
+let login = prompt ("Please enter your login name", "User");
+let email = prompt ("Please enter your email name", "user@gmail.com");
+let password = prompt ("Please enter your password for user email", "zxcvbn");
+
+console.log("Dear " + login + ", your email is " + email + ", your password is " + password);
+alert("Dear " + login + ", your email is " + email + ", your password is " + password);
+
+// task8
+
+const secInHour = 60 * 60;
+const secInDateNight = secInHour * 24;
+let daysInMonth = prompt("How many days are in the current month?")
+let secInMonth;
+secInMonth = daysInMonth * secInDateNight;
+console.log(secInHour, secInDateNight, secInMonth);
+alert("An hour has " + secInHour + " seconds. " + "Date and night has " + secInDateNight + " seconds. " + "Current month has " + secondsInMonth + " seconds.");
+
+
